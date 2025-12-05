19df6d4250f8e08b2ede74121941df080372cede
suvadeep <suvadeepm0709@gmail.com>
2025-12-05 11:20:24 +0530

    Add lambda

--- lib/cute-cats-serverless-stack.ts
+++ lib/cute-cats-serverless-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import { Bucket } from "aws-cdk-lib/aws-s3";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
 
 export class CuteCatsServerlessStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -11,6 +13,16 @@ const catsBucket = new Bucket(this, "CatsBucket", {
   bucketName: `cute-cats-upload-${this.account}`,
   versioned: false,
 });
+
+const uploadFunction = new NodejsFunction(this, "UploadCatFunction", {
+  entry: "lambda/upload-handler.ts",
+  runtime: Runtime.NODEJS_18_X,
+  environment: {
+    CATS_BUCKET: catsBucket.bucketName,
+  },
+});
+
+catsBucket.grantPut(uploadFunction);
 
 
     
